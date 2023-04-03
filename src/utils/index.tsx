ef585bfc8621f82c0c8cc45376b33f68a3e9e614
Daniel Broe <BroeDaniel@gmail.com>
2023-04-03 14:15:17 +0200

    sort by date typing

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,12 +1,11 @@
-// type Post = {
-//   slug: string;
-//   frontmatter: {
-//     [key: string]: any;
-//     date: string;
-//   };
-// };
+type Post = {
+  slug: string;
+  frontmatter: {
+    [key: string]: any;
+  };
+};
 
-export const sortByDate = (a: any, b: any) => {
+export const sortByDate = (a: Post, b: Post) => {
   const aDate: Date = new Date(a.frontmatter.date);
   const bDate: Date = new Date(b.frontmatter.date);
   return bDate.getTime() - aDate.getTime();
