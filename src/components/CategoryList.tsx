4f8add61f3ab44f189145f5074d9f35049831ac0
Daniel Broe <BroeDaniel@gmail.com>
2023-04-11 13:00:33 +0200

    adding category lists

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 
-export default function CategoryList({ categories }) {
+export default function CategoryList({ categories }: any) {
   return (
     <div className='w-full p-5 bg-white rounded-lg shadow-md mt-6'>
       <h3 className='text-2xl bg-gray-800 text-white p-3 rounded'>
         Blog Categories
       </h3>
       <ul className='divide-y divide-gray-300'>
-        {categories.map((category, index) => (
+        {categories.map((category: any, index: any) => (
           <Link key={index} href={`/blog/category/${category.toLowerCase()}`}>
             <li className='p-4 cursor-pointer hover:bg-gray-50'>{category}</li>
           </Link>
