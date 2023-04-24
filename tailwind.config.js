2a91c35c96ef17ee5be67c360e3ebe4df90cefb5
Daniel Broe <BroeDaniel@gmail.com>
2023-04-24 11:15:25 +0200

    xs breakpoint added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '375px',
+      },
+    },
   },
   variants: {
     extend: {},
